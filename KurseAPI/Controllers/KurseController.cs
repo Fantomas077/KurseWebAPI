9e81a0f6e9a330932f6aaedb845d776fcbd6bf8d
Fantomas077 <nheltonn@gmail.com>
2025-11-16 22:51:18 +0100

    Projektdateien hinzufügen.

---
+++ KurseAPI/Controllers/KurseController.cs
@@ -0,0 +1,62 @@
+﻿using KurseAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KurseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KurseController : ControllerBase
+    {
+        private static List<Kurse> KurseListe = new List<Kurse>()
+        {
+            new Kurse(){Id=1,Name="Französisch für Anfänger",Description="Französisch lernen A1-A2",Score=15},
+            new Kurse(){Id=2,Name="Python ",Description="Python Kurse",Score=15},
+            new Kurse(){Id=3,Name="Mathe für Anfänger",Description="Mathe lernen ",Score=10},
+            new Kurse(){Id=4,Name="Data Science für Anfänger",Description="Data Science lernen ",Score=11}
+        };
+
+        [HttpGet]
+        public IEnumerable<Kurse> Get()
+        {
+            return KurseListe;
+        }
+        [HttpGet("{id}")]
+        public Kurse Get(int id)
+        {
+            var pos = KurseListe.Find(x => x.Id == id);
+            return pos;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Kurse kurse)
+        {
+            KurseListe.Add(kurse);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var pos = KurseListe.Find(x => x.Id == id);
+            if (pos != null)
+            {
+                KurseListe.Remove(pos);
+            }
+
+
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Kurse kurse)
+        {
+            var pos = KurseListe.Find(x => x.Id == id);
+            if (pos != null)
+            {
+                pos.Name = kurse.Name;
+                pos.Score = kurse.Score;
+                pos.Description = kurse.Description;
+
+            }
+
+
+        }
+
+    }
+}
---
+++ KurseAPI/Models/Kurse.cs
@@ -0,0 +1,10 @@
+﻿namespace KurseAPI.Models
+{
+    public class Kurse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public string Description { get; set; }
+    }
+}
