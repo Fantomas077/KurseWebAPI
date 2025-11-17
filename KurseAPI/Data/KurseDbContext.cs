e09b7def3e00d480627b4d1f8332d9391425b1f3
Fantomas077 <nheltonn@gmail.com>
2025-11-17 21:16:19 +0100

    AddDbContext

---
+++ KurseAPI/Data/KurseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using KurseAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace KurseAPI.Data
+{
+    public class KurseDbContext : DbContext
+    {
+        public KurseDbContext(DbContextOptions<KurseDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Kurse> Kurses { get; set; }
+    }
+}
