4f761e00b0557198f75b8687ac57f5201226bb85
Fantomas077 <nheltonn@gmail.com>
2025-11-17 13:48:23 +0100

    StatutsCode hinzufügen

--- KurseAPI/Controllers/KurseController.cs
+++ KurseAPI/Controllers/KurseController.cs
@@ -9,42 +9,66 @@ namespace KurseAPI.Controllers
     {
         private static List<Kurse> KurseListe = new List<Kurse>()
         {
-            new Kurse(){Id=1,Name="Französisch für Anfänger",Description="Französisch lernen A1-A2",Score=15},
+             new Kurse(){Id=1,Name="Französisch für Anfänger",Description="Französisch lernen A1-A2",Score=15},
             new Kurse(){Id=2,Name="Python ",Description="Python Kurse",Score=15},
-            new Kurse(){Id=3,Name="Mathe für Anfänger",Description="Mathe lernen ",Score=10},
+             new Kurse(){Id=3,Name="Mathe für Anfänger",Description="Mathe lernen ",Score=10},
             new Kurse(){Id=4,Name="Data Science für Anfänger",Description="Data Science lernen ",Score=11}
         };
 
         [HttpGet]
-        public IEnumerable<Kurse> Get()
+        public IActionResult Get()
         {
-            return KurseListe;
+            if (KurseListe.Count == 0)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, "Keine Kurse vorhanden");
+
+            }
+            return Ok(KurseListe);
         }
+
+
+
         [HttpGet("{id}")]
-        public Kurse Get(int id)
+        public IActionResult Get(int id)
         {
             var pos = KurseListe.Find(x => x.Id == id);
-            return pos;
+            if (pos == null)
+            {
+                return NotFound("keine gefunden");
+
+
+            }
+            return Ok(pos);
         }
 
         [HttpPost]
-        public void Post([FromBody] Kurse kurse)
+        public IActionResult Post([FromBody] Kurse kurse)
         {
+            var exist = KurseListe.Find(x => x.Id == kurse.Id);
+            if (exist != null)
+            {
+                return BadRequest("diese kurse existiert schon");
+            }
             KurseListe.Add(kurse);
+            return Created($"/api/kurse/{kurse.Id}", kurse);
+
+
         }
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
             var pos = KurseListe.Find(x => x.Id == id);
             if (pos != null)
             {
                 KurseListe.Remove(pos);
+                return Ok($"{pos.Name} wurde erfolgreich gelöscht");
             }
+            return NotFound("wurde nicht gefunden");
 
 
         }
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Kurse kurse)
+        public IActionResult Put(int id, [FromBody] Kurse kurse)
         {
             var pos = KurseListe.Find(x => x.Id == id);
             if (pos != null)
@@ -52,9 +76,10 @@ namespace KurseAPI.Controllers
                 pos.Name = kurse.Name;
                 pos.Score = kurse.Score;
                 pos.Description = kurse.Description;
+                return Ok("Der Kurse Wurde erfolgreich updated");
 
             }
-
+            return NotFound("wurde nicht gefunden");
 
         }
 
--- KurseAPI/Models/Kurse.cs
+++ KurseAPI/Models/Kurse.cs
@@ -1,10 +1,16 @@
-﻿namespace KurseAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KurseAPI.Models
 {
     public class Kurse
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is pflicht")]
         public string Name { get; set; }
+        [Range(0, 20, ErrorMessage = "Score beetween 0 and 20")]
         public int Score { get; set; }
+        [MaxLength(50, ErrorMessage = "Max 20 Charakter")]
+        [Required]
         public string Description { get; set; }
     }
 }
